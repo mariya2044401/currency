5bec1165d10fc3ee3fd990c132757fdcb7d80ddd
Mariya Khan <jiyarehman80@gmail.com>
2024-04-13 15:51:55 -0700

    second attempt

---
+++ main.js
@@ -0,0 +1,40 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+import chalk from "chalk";
+console.log(chalk.bold.yellowBright("$$$<<<====>>>CURRENCY CONVERTER<<<===>>>$$$"));
+const currency = {
+    USD: 1,
+    Euro: 0.94,
+    GBP: 0.8,
+    INR: 83.61,
+    PKR: 277,
+};
+let user_answer = await inquirer.prompt([
+    {
+        name: "from",
+        type: "list",
+        message: "Enter from currency",
+        choices: ["USD", "Euro", "GBP", "INR", "PKR"],
+    },
+    {
+        name: "to",
+        type: "list",
+        message: "Enter to currency",
+        choices: ["USD", "Euro", "GBP", "INR", "PKR"],
+    },
+    {
+        name: "amount",
+        type: "number",
+        message: "Enter your amount",
+    }
+]);
+//   console.log(user_answer);
+let fromAmount = currency[user_answer.from]; //exchnge rate
+let toAmount = currency[user_answer.to]; //exchnge rate
+let amount = user_answer.amount;
+let baseAmount = amount / fromAmount; //USD BASE CURRENCY  1000/1 usd
+let convertedAmount = baseAmount * toAmount;
+console.log(fromAmount);
+console.log(toAmount);
+console.log(amount);
+console.log(convertedAmount);
